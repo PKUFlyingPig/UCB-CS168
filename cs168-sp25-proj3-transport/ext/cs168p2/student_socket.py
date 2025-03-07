ebe5ac30ba4a1c81d98587411a0c195a4ee5aa8d
PKUFlyingPig <zhongyinmin@pku.edu.cn>
2025-03-08 00:34:23 +0800

    p3: s5

--- cs168-sp25-proj3-transport/ext/cs168p2/student_socket.py
+++ cs168-sp25-proj3-transport/ext/cs168p2/student_socket.py
@@ -719,10 +719,9 @@ class StudentUSocket(StudentUSocketBase):
     """
 
     ## Start of Stage 5.1 ##
-    self.snd.wnd = self.TX_DATA_MAX # remove when implemented
+    self.snd.wnd = seg.win
     self.snd.wl1 = seg.seq
     self.snd.wl2 = seg.ack
-
     ## End of Stage 5.1 ##
 
   def handle_accepted_ack(self, seg):
